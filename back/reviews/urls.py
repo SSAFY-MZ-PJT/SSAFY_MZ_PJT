7796faad0b5ec01f3ae4332503e10ce118caf82b
Jaehyeok Kim <rublin322@gmail.com>
2024-11-20 22:50:47 +0900

    리뷰가 영화와 바로 연결되도록 수정

--- back/reviews/urls.py
+++ back/reviews/urls.py
@@ -6,9 +6,10 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-    path('', views.review_list_create, name='review_list_create'),
-    path('<int:review_pk>/', views.review_detail, name='review_detail'),
-    path('<review_pk>/comments/', views.comment_create, name='comment_create'),
-    path('<review_pk>/comments/<comment_pk>/', views.comment_detail, name='comment_detail'),
+    path('', views.all_review_list, name='all_review_list'), # 전체 리뷰 조회
+    path('<int:movie_pk>/', views.movie_review_list_create, name='movie_review_list_create'), # 특정 영화에 대한 리뷰 리스트 조회 (GET) 및 특정 영화에 대한 리뷰 작성 (POST)
+    path('<int:review_pk>/', views.review_detail, name='review_detail'), # 리뷰 상세 조회, 리뷰 삭제, 리뷰 수정
+    path('<int:review_pk>/comments/', views.comment_create, name='comment_create'),
+    path('<int:review_pk>/comments/<int:comment_pk>/', views.comment_detail, name='comment_detail'),
     path('like/<int:review_pk>/', views.review_like, name='review_like'),
 ]
